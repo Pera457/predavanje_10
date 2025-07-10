563d12f105b7074ea07d3424a91199728f67a262
Pera457 <petarpfend@gmail.com>
2025-07-10 12:46:42 +0200

    Finished

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 import json
 
-with open ("data.json",'r') as file:
+with open ("folder/user.json", 'r') as file:
     data = json.load(file)
     data.append({
         "name" : "Petar petrovic",
@@ -12,5 +12,5 @@ with open ("data.json",'r') as file:
 
 print(data)
 
-with open("data.json", 'w') as file:
+with open("folder/user.json", 'w') as file:
     json.dump(data, file, indent = 4)

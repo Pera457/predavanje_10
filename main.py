c1d29640588199d745fb009b9a12087e4a219d8e
Pera457 <petarpfend@gmail.com>
2025-07-10 12:25:46 +0200

    Finished

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 
-# test
+import json
+
+with open ("data.json",'r') as file:
+    data = json.load(file)
+    data.append({
+        "name" : "Petar petrovic",
+        "age" : 22,
+        "height" : 190,
+        "gender" : "male"
+    })
+
+print(data)
+
+with open("data.json", 'w') as file:
+    json.dump(data, file, indent = 4)
